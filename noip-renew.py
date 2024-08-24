d4d6960e1da1cb05e50118ffd0f964c156550c7d
dhp5621 <daohongphuc1112010@hotmail.com>
2024-08-24 10:16:50 +0700

    Update noip-renew.py

--- noip-renew.py
+++ noip-renew.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+PYTHON -m pip install selenium
 
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
